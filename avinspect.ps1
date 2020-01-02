c06fa5e36ffeeed9cd4e1993b6f7ce5208ad077c
Marc Schneider <45066148+Mar8x@users.noreply.github.com>
2020-01-02 20:41:59 +0100

    Update avinspect.ps1

--- avinspect.ps1
+++ avinspect.ps1
@@ -1,5 +1,5 @@
 
-<# © 2019, AppGate, Inc.  All rights reserved.
+<# © 2019, AppGate.  All rights reserved.
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met: (a) redistributions
 of source code must retain the above copyright notice, this list of conditions and
